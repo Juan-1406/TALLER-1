4daaabfccbb710c24e0b51d3d4eb53bdbff2ba76
Juan-1406 <juan.alvarado01@alumnos.ucn.cl>
2025-09-06 21:55:52 -0400

    Creación clase Curso
    Se crea la clase curso con sus respectivos atributos en su formato .h y .cpp

---
+++ Curso.cpp
@@ -0,0 +1,27 @@
+#include "Curso.h"
+#include <iostream>
+using namespace std;
+
+Curso::Curso(const string &codigo, const string &nombre, int cantMaxima, const string &carrera, const string &profesor) {
+    this -> codigo = codigo;
+    this -> nombre = nombre;
+    this -> cantMaxima = cantMaxima;
+    this -> carrera = carrera;
+    this -> profesor = profesor;
+}
+
+string Curso::getCodigo() const {return codigo;}
+string Curso::getNombre() const {return nombre;}
+int Curso::getCantMaxima() const {return cantMaxima;}
+string Curso::getCarrera() const {return carrera;}
+string Curso::getProfesor() const {return profesor;}
+
+void Curso::mostrarInfo() {
+    cout << "Codigo: " << codigo << " | " << nombre << " | Max de Estudiantes: " << cantMaxima << " | Carrera: " << carrera << " | Profesor: " << profesor << endl;
+}
+
+
+
+
+
+
---
+++ Curso.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class Curso {
+    protected:
+        string codigo;
+        string nombre;
+        int cantMaxima;
+        string carrera;
+        string profesor;
+    public:
+        Curso(const string& codigo, const string& nombre, int cantMaxima, const string& carrera, const string& profesor); //CONSTRUCTOR
+
+        string getCodigo() const;
+        string getNombre() const;
+        int getCantMaxima() const;
+        string getCarrera() const;
+        string getProfesor() const;
+
+        void mostrarInfo();
+
+    ~Curso(){}
+};
