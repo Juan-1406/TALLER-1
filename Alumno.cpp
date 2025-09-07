8ee96012e17e623eb6315ce61b7d2c6cb67999aa
Juan-1406 <juan.alvarado01@alumnos.ucn.cl>
2025-09-06 21:06:20 -0400

    Creación clase alumno
    Se creo la clase alumno con sus atributos correspondientes, en su formato .h y .cpp

---
+++ Alumno.cpp
@@ -0,0 +1,21 @@
+#include "Alumno.h"
+#include <iostream>
+using namespace std;
+
+Alumno::Alumno(int id, const string &nombre, const string &apellido, const string &carrera, const string &fechaIngreso) {
+    this -> id = id;
+    this -> nombre  = nombre;
+    this -> apellido = apellido;
+    this -> carrera = carrera;
+    this -> fechaIngreso = fechaIngreso;
+}
+
+int Alumno::getId() const {return id;}
+string Alumno::getNombre() const {return nombre;}
+string Alumno::getApellido() const {return apellido;}
+string Alumno::getCarrera() const {return carrera;}
+string Alumno::getFechIngreso() const {return fechaIngreso;}
+
+void Alumno::mostrarInfo() {
+    cout << "ID: " << id << " | " << nombre << " | " << apellido << " | Carrera: " << carrera << " | Fecha Ingreso: " << fechaIngreso << endl;
+}
---
+++ Alumno.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class Alumno {
+    protected:
+        int id;
+        string nombre;
+        string apellido;
+        string carrera;
+        string fechaIngreso;
+    public:
+        Alumno(int id, const string& nombre, const string& apellido, const string& carrera, const string& fechaIngreso); //CONSTRUCTOR
+
+        int getId() const;
+        string getNombre() const;
+        string getApellido() const;
+        string getCarrera() const;
+        string getFechIngreso() const;
+
+        void mostrarInfo();
+
+    ~Alumno(){}
+};
