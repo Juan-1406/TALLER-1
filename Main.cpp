d267a218c0adc622eb7fa72b21ac9e43ddf23035
Juan-1406 <juan.alvarado01@alumnos.ucn.cl>
2025-09-09 23:57:20 -0300

    Actualizacion Main
    Se actualizan ciertos funcionamientos que tienen que ver con algunos menus creaados

--- Main.cpp
+++ Main.cpp
@@ -7,10 +7,9 @@ using namespace std;
 ListAlumnos lista_alumnos;
 ListCursos lista_cursos;
 
-int opcion; int segOpcion; int id;
+int opcion; int segOpcion; int id; int codigo;
 
 void manejoAlumnos() {
-    int opcion;
     do {
         cout << endl << "--- MANEJO DE ALUMNOS ---" << endl;
         cout << "1. Crear Alumno" << endl << "2. Buscar Alumno" << endl << "3. Eliminar Alumno" << endl << "0. Salir" << endl;
@@ -60,8 +59,7 @@ void manejoAlumnos() {
 
                 } else if (segOpcion == 2) {
                     string nombre;
-                    cout << "Nombre:";
-                    cin >> nombre;
+                    cout << "Nombre:"; cin >> nombre;
                     lista_alumnos.mostrarNombres(nombre);
                 }
                 cout << endl;
@@ -92,35 +90,50 @@ void manejoCursos() {
                 break;
             case 1: {
                 int cantMaxima;
-                string codigo; string nombre; string carrera; string profesor;
+                string nombre; string carrera; string profesor;
 
                 cout << "Codigo:"; cin >> codigo;
+                if (lista_cursos.porCodigo(codigo)) {
+                    cout << endl << "Ya existe un curso con ese codigo" << endl;
+                    continue;
+                }
+
                 cout << "Nombre:"; cin >> nombre;
                 cout << "Cantidad Maxima:"; cin >> cantMaxima;
                 cout << "Carrera:"; cin >> carrera;
                 cout << "Profesor:"; cin >> profesor;
 
-                //Curso* curso = new Curso(codigo, nombre, cantMaxima, carrera, profesor);
+                Curso* curso = new Curso(codigo, nombre, cantMaxima, carrera, profesor);
+                lista_cursos.agregarCurso(curso);
+                cout << endl << "Curso creado con exito!" << endl;
                 break;
             }
+
             case 2: {
-                int segOpcion;
-                cout << "1. Por ID" << endl << "2. Por Nombre" << endl << "Ingrese una opcion:";
+                cout << "1. Por Codigo" << endl << "2. Por Nombre" << endl << "Ingrese una opcion:";
                 cin >> segOpcion;
 
                 if (segOpcion == 1) {
-                    int id;
-                    cout << "ID:";
-                    cin >> id;
+                    cout << "Codigo:"; cin >> codigo;
+                    if (!lista_cursos.porCodigo(codigo)) {
+                        cout << endl << "Curso no encontrado" << endl;
+                        continue;
+                    }
 
-                } else if (segOpcion == 2) {
+                    Curso* curso = lista_cursos.porCodigo(codigo);
+                    curso -> mostrarInfo();
+
+                } else if(segOpcion == 2) {
                     string nombre;
-                    cout << "Nombre:";
-                    cin >> nombre; cout << nombre;
+                    cout << "Nombre:"; cin >> nombre;
+                    lista_cursos.porNombre(nombre);
                 }
+                cout << endl;
                 break;
             }
             case 3: {
+                cout << "Ingrese CODIGO a eliminar:"; cin >> codigo;
+                lista_cursos.eliminarCurso(codigo);
                 break;
             }
         }
@@ -128,9 +141,9 @@ void manejoCursos() {
 }
 
 void manejoInscrip() {
-    int opcion;
 
     do {
+        cout << endl << "--- MANEJO DE INSCRIPCIONES ---" << endl;
         cout << "1. Inscribir alumno a un curso" << endl << "2. Eliminar alumno de un curso" << endl << "0. Salir" << endl;
         cout << "Ingrese una opcion:";
         cin >> opcion;
@@ -155,7 +168,39 @@ void manejoNotas() {
 }
 
 void consultReport() {
+    do {
+        cout << endl << "--- CONSULTAS Y REPORTES ---" << endl;
+        cout << "1. Alumnos de una carrera " << endl << "2. Todos los cursos de un alumno" << endl << "3. Promedio de un alumno (1 curso)";
+        cout << endl << "4. Promedio general de un alumno" << endl << "0. Salir" << endl;
+        cout << "Ingrese una opcion:";
+        cin >> opcion;
 
+        switch(opcion) {
+            case 0:
+                cout << endl;
+                break;
+            case 1: {
+                string carrera;
+                cout << "Ingrese la carrera:"; cin >> carrera;
+                lista_alumnos.mostrarAlumnosCarrera(carrera);
+                cout << endl;
+                break;
+            }
+
+            case 2: {
+
+                break;
+            }
+            case 3: {
+
+            }
+            case 4: {
+
+                break;
+            }
+
+        }
+    } while(opcion >= 1 && opcion <= 4);
 }
 
 int main() {
@@ -171,7 +216,7 @@ int main() {
 
         switch(opcion) {
             case 0:
-                cout << "Saliste con exito!";
+                cout << endl << "Saliste con exito!";
                 break;
             case 1:
                 manejoAlumnos();
